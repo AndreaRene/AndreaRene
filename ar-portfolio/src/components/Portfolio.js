d24287f45690afef8069c39eb09fb2b6786d88da
Andrea <andrea@interhact.net>
2022-11-08 17:29:34 -0600

    work on data array in project.js to add more projects to the application

--- ar-portfolio/src/components/Portfolio.js
+++ ar-portfolio/src/components/Portfolio.js
@@ -1,7 +1,8 @@
 import Project from './Project'
 import './Portfolio.css'
 import totr from '../assets/totr.png'
-import pom from '../assets/pom.png'
+import quickweather from '../assets/quickweather.jpeg';
+import quizly from '../assets/quizly.png'
 
 const data = [
     {
@@ -14,14 +15,29 @@ const data = [
         altText: 'a small sample shot of the cocktail application',
     },
     {
+        descriptionOne: "Here's a lightweight weather dashboard I've put together for some simple information about the weather in any city you can think of. Currently it only searches by city so you will get a surprise!  But keep an eye out for state and country selectors soon! This app looks amazing on mobile so try it out next time you're traveling!",
+        caption: 'Quick Weather',
+        projectUrl: 'https://andrearene.github.io/Weather_App/',
+        imageSource: quickweather,
+        altText: 'small image of weather app'
+    },
+    {
+        descriptionOne: "I tried my hand at this little quiz app. Right now it's just five simple questions but I have many ideas about how it could be used.",
+        descriptionTwo: "The thing I LOVED about developing this was building it in a way that scaling it and fitting it to any subject is extremely simple. I look forward to digging into it further to find more performant ways to build this in the future!",
+        caption: 'Quizly',
+        projectUrl: 'https://andrearene.github.io/Development_Quiz_App/',
+        imageSource: quizly,
+        altText: 'small image of quiz app',
+    },
+    {
         descriptionOne: 'The app allows the user to either find a recipe for a random cocktail or a specific one by searching by name or ingredient.',
         descriptionTwo: 'It is fully responsive from XL to mobile devices so take it with you to your next get together and have fun!',
         descriptionThree: 'Please drink responsibly and Happy Mixing!',
         caption: 'Trivia on the rocks.',
         projectUrl: 'https://andrearene.github.io/Trivia_on_the_rocks/',
         imageSource: totr,
-        altText: 'a small sample shot of the cocktail application'
-    }
+        altText: 'a small sample shot of the cocktail application',
+    },
 ]
 
 const Portfolio = () => {
