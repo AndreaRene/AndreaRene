86f09ba3bb3dbe7446bee9641c45e67f55edf259
Andrea <andrea@interhact.net>
2022-11-15 13:02:25 -0600

    work on layout and design in footer and resume

--- ar-portfolio/src/components/Footer.js
+++ ar-portfolio/src/components/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
     return (
         <footer id="pageFooter">
             <section id="contact">
-                <a href="https://github.com/AndreaRene" target="_" ><img src={github} className="contactIcon" alt="github icon" /></a>
-                <a href="https://www.linkedin.com/in/andrea-presto-a6a6b223a/" target="_" className="contactClick"><img src={linkedin} className="contactIcon" alt="linkedin icon" /></a>
+                <a href="https://github.com/AndreaRene" target="_" ><i class="devicon-github-original contactIcon"></i></a>
+                <a href="https://www.linkedin.com/in/andrea-presto-a6a6b223a/" target="_" className="contactClick"><i className="devicon-linkedin-plain contactIcon" alt="linkedin icon" /></a>
                 <a href="https://stackoverflow.com/users/19103386/andrea" target="_" className="contactClick"><img src={so} className="contactIcon" alt="stackoverflow icon" /></a>
                 <a href="mailto:andrea@interhact.net" target="_" ><img src={email} className="contactIcon" alt="email message icon" /></a>
                 <a href="tel:5129544257" ><img src={cell} className="contactIcon" alt="cell phone icon" /></a>
--- ar-portfolio/src/components/Resume.js
+++ ar-portfolio/src/components/Resume.js
@@ -1,31 +1,10 @@
 import './Resume.css';
+import miro from '../assets/miro.png';
 import resume from '../assets/AndreaReneResume.pdf';
 
 const Resume = () => {
     return (
         <main className='mainSection resumePage'>
-            <p>Platforms:</p>
-            <div className='icons'>
-
-                <i className="devicon-github-original colored"></i>
-                <i className="devicon-heroku-plain colored"></i>
-            </div>
-            <p>Languages:</p>
-            <div className='icons'>
-
-                <i className="devicon-html5-plain colored"></i>
-                <i className="devicon-css3-plain colored"></i>
-                <i className="devicon-javascript-plain"></i>
-            </div>
-            <p>Frameworks & Libraries:</p>
-            <div className='icons'>
-
-                <i className="devicon-bootstrap-plain colored"></i>
-                <i className="devicon-jquery-plain colored"></i>
-                <i className="devicon-express-original colored"></i>
-                <i className="devicon-react-original"></i>
-            </div>
-
             <p>Technologies:</p>
             <div className='icons'>
                 <i className="devicon-handlebars-plain colored"></i>
@@ -36,6 +15,37 @@ const Resume = () => {
                 <i className="devicon-mongodb-plain colored"></i>
                 <i className="devicon-graphql-plain colored"></i>
             </div>
+            <p>Frameworks & Libraries:</p>
+            <div className='icons'>
+
+                <i className="devicon-bootstrap-plain colored"></i>
+                <i className="devicon-jquery-plain colored"></i>
+                <i className="devicon-express-original colored"></i>
+                <i className="devicon-react-original"></i>
+            </div>
+            <p>Languages:</p>
+            <div className='icons'>
+
+                <i className="devicon-html5-plain colored"></i>
+                <i className="devicon-css3-plain colored"></i>
+                <i className="devicon-javascript-plain"></i>
+            </div>
+            <p>Platforms:</p>
+            <div className='icons'>
+
+                <i className="devicon-github-original colored"></i>
+                <i className="devicon-heroku-plain colored"></i>
+            </div>
+            <p>Tools:</p>
+            <div className='icons'>
+
+                <i class="devicon-vscode-plain"></i>
+                <img src={miro} />
+            </div>
+
+
+
+
 
             <a id='downloadResume' download='AndreaReneResume.pdf' href={resume}>Download my resume!</a>
         </main>
