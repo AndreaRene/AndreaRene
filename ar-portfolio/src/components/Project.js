5ee473516118d39e5222e1a70a1ea61733b419dc
Andrea <andrea@interhact.net>
2022-11-15 17:11:12 -0600

    removed wonky count if statements from project.css

--- ar-portfolio/src/components/Project.js
+++ ar-portfolio/src/components/Project.js
@@ -1,18 +1,18 @@
 import './Project.css';
 
 const Project = ({ descriptionOne, descriptionTwo, descriptionThree, caption, projectUrl, imageSource, altText, githubUrl, sections }) => {
-    let count = 0;
-    if (sections === 0) {
-        count = 1;
-    } else if (sections === 1) {
-        count = 2;
-    } else if (sections === 2) {
-        count = 3;
-    } else if (sections === 3) {
-        count = 4;
-    }
+    // let count = 0;
+    // if (sections === 0) {
+    //     count = 1;
+    // } else if (sections === 1) {
+    //     count = 2;
+    // } else if (sections === 2) {
+    //     count = 3;
+    // } else if (sections === 3) {
+    //     count = 4;
+    // }
     return (
-        <section className={'boxGroup boxSection' + (count)}>
+        <section className={'boxGroup boxSection' + (sections + 1)}>
             <div className={'box'}>
                 <p>{descriptionOne}</p>
                 <p>{descriptionTwo}</p>
@@ -31,3 +31,32 @@ const Project = ({ descriptionOne, descriptionTwo, descriptionThree, caption, pr
 }
 
 export default Project;
+
+// const numbers = [1, 2, 3, 4];
+// return (
+//     <div>
+//         {
+//             numbers.map((item) => {
+//                 return (
+//                     <section className={'boxGroup boxSection' + (item)}>
+//                         <div className={'box'}>
+//                             <p>{descriptionOne}</p>
+//                             <p>{descriptionTwo}</p>
+//                             <p>{descriptionThree}</p>
+//                             <p><a href={githubUrl} target="_">
+//                                 <i class="devicon-github-original"></i>
+//                             </a></p>
+//                         </div>
+
+//                         <figure className='box'>
+//                             <a href={projectUrl} target="_"><img src={imageSource}
+//                                 alt={altText} /></a>
+//                             <figcaption>{caption}</figcaption>
+//                         </figure>
+//                     </section>
+
+//                 )
+//             })
+//         }
+//     </div>
+// )
